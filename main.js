0fab2e1a5fa578419f4a97d29b81981d3ef5838f
bazzscript <bmexit@gmail.com>
2022-05-19 10:44:17 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let itemName = document.getElementById('item_name');
+
+console.log(itemName);
+
+let desc = document.getElementById('description').value;
+
+let fname = itemName[0].value;
+
+document.getElementById("td1").innerHTML = fname ;
+
